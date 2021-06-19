abb3edc2d5f67740b93262bcdeb874cf2e835e2b
Jaid <jaid.jsx@gmail.com>
2021-06-19 19:16:36 +0200

    improve: Added debug log
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import insertStringBefore from "insert-string-before"
 
 import injectionTemplate from "./injection.hbs"
 
+import debug from "lib/debug"
+
 /**
  * @typedef {Object} Options
  * @prop {number} [port = 3000]
@@ -30,12 +32,15 @@ export default class InjectBrowserSyncPlugin {
    * @param {import("webpack").Compiler} compiler
    */
   apply(compiler) {
+    debug("Options: %o", this.options)
     const injection = injectionTemplate({
       options: this.options,
     })
     compiler.hooks.compilation.tap(process.env.REPLACE_PKG_NAME, compilation => {
       HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tap(process.env.REPLACE_PKG_NAME, data => {
+        debug("Before: %s", data.html)
         data.html = insertStringBefore(data.html, "</body>", injection)
+        debug("After: %s", data.html)
       })
     })
   }
